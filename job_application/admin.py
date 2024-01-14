06f581f3cf3fbb005433681be379350c75a681f4
Vishwaas H Kumar <iamvishwaas.hkumar@gmail.com>
2024-01-14 22:12:33 +0530

    Commit #3

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Form
 
-# Register your models here.
+#modifying Forms section in Admin
+class FormAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name', 'email')
+    list_filter = ('date', 'occupation')
+    ordering = ('first_name',) #-first_name for reverse
+    readonly_fields = ('occupation',)
+
+#registering class Form with admin interface
+admin.site.register(Form, FormAdmin)
--- job_application/views.py
+++ job_application/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
+from .models import Form
+from django.contrib import messages
+from django.core.mail import EmailMessage
 
 #creating views
 
 def index(request):
+
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            occupation = form.cleaned_data["occupation"]
+
+            #storing data in the database
+            Form.objects.create(first_name=first_name, last_name=last_name,
+                                email=email, date=date, occupation=occupation)
+
+            #sending confirmation email
+            message_body = f"A new Job Application was submitted. Thank you, {first_name}"
+            email_message = EmailMessage("Form submission confirmation",message_body, to=[email])
+            email_message.send()
+            #to set where Django sends email from...configure in settings.py
+
+            messages.success(request, "Form Submitted Successfully")
     return render(request, "index.html")
