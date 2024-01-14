2c6ea92a512a082115f94068e1a57e0f5803d64c
Vishwaas H Kumar <iamvishwaas.hkumar@gmail.com>
2024-01-14 20:33:45 +0530

    Commit #2

--- job_application/models.py
+++ job_application/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+#designing the database
+
+#designing Form table
+class Form(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    date = models.DateField()
+    occupation = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
